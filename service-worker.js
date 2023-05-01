92cbda491e7852e3c4b0da7ecf72ec82377e822a
Troy Lemons Jr <troylemonsjr@gmail.com>
2023-05-01 14:35:49 -0700

    revamped swjs

--- service-worker.js
+++ service-worker.js
@@ -1,65 +1,48 @@
-const cacheName = "htmlCSShw4";
-const assets = [
-"./",
-"./index.html",
-"./css/style.css",
-"./js/hamburger.js",
-"./js/main.js",
-"./js/manifest.json",
-"./images/lightblue.jpg",
-"./images/lightgold.jpg",
-"./images/gold.jpeg",
-"./images/pwwa.png"
-];
-
-/* for installing event listener */
-// self.addEventListener("install", function (e) {
-//     e.waitUntil(
-//       caches.open(cacheName).then(function (cache) {
-//         return cache.add(assets);
-//       })
-//     );
-//   });
-
-/* for fetching assets */
-// self.addEventListener("fetch", function (e) {
-//   console.log(e.request.url);
-//     e.respondWith(
-//       caches.match(e.request).then(function(response) {
-//         return response || fetch(e.request);
-//       })
-//     );
-//   });
+// const cacheName = "htmlCSShw4";
+// const assets = [
+// "./",
+// "./index.html",
+// "./css/style.css",
+// "./js/hamburger.js",
+// "./js/main.js",
+// "./js/manifest.json",
+// "./images/lightblue.jpg",
+// "./images/lightgold.jpg",
+// "./images/gold.jpeg",
+// "./images/pwwa.png"
+// ];
 
 
-
-self.addEventListener("install", event => {
-  console.log("installing...");
+self.addEventListener('install', function(event) {
   event.waitUntil(
-      caches
-          .open(cacheName)
-          .then(cache => {
-              return cache.addAll(assets);
-          })
-          .catch(err => console.log(err))
-  );
+    caches.open('cache').then(function(cache) {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./css/style.css",
+        "./js/hamburger.js",
+        "./js/main.js",
+        "./js/manifest.json",
+        "./images/lightblue.jpg",
+        "./images/lightgold.jpg",
+        "./images/gold.jpeg",
+        "./images/pwwa.png"
+       ]);
+    })
+   );
 });
 
-self.addEventListener("fetch", event => {
-  if (event.request.url === "https://fanciful-praline-5f0d16.netlify.app") {
-      // or whatever your app's URL is
-      event.respondWith(
-          fetch(event.request).catch(err =>
-              self.cache.open(cacheName).then(cache => cache.match(assets))
-          )
-      );
-  } else {
-      event.respondWith(
-          fetch(event.request).catch(err =>
-              caches.match(event.request).then(response => response)
-          )
-      );
-  }
+self.addEventListener('fetch', function(event){
+  event.respondWith(async function () {
+     var cache = await caches.open('cache');
+     var cachedResponsePromise = await cache.match(event.request);
+     var networkResponsePromise = fetch(event.request);
+     event.waitUntil(async function () {
+        var networkResponse = await networkResponsePromise;
+        await cache.put(event.request, networkResponse.clone());
+     }());
+     return cachedResponsePromise || networkResponsePromise;
+   }());
 });
 
 
